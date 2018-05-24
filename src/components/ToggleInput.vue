025e54aa980123b768339e3c7d63b8c55ca10b43
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-05-24 14:58:36 +0300

    Создадим компонент Скопируем шаблон Объявим локальные данные и методы

---
+++ src/components/ToggleInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="toggle flex-no-shrink" role="checkbox" tabindex="0"
+    :aria-checked="receiveNewsletter.toString()"
+    @click="toggleNewsletter"
+    @keydown.space.prevent="toggleNewsletter"
+  ></span>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value: false
+      }
+    },
+    methods: {
+      toggle() {
+        this.value = !this.value
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
